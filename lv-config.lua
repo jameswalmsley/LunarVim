9329c1cafe156f184968539b997e22bd1272c4f2
James Walmsley <james@fullfat-fs.co.uk>
2021-10-08 17:12:43 +0100

    jw settings.

--- lv-config.lua
+++ lv-config.lua
@@ -1,6 +1,18 @@
 lvim.builtin.dashboard.active = true
 lvim.builtin.nvimtree.side = "right"
-lvim.format_on_save = false
+lvim.format_on_save = true
+lvim.lang.cpp.formatters = {
+  {
+    exe = 'clang_format',
+    args = {},
+    opts = {
+      cwd = vim.fn.expand("%:p:h")
+    }
+  }
+}
+
+lvim.keys.normal_mode["<C-j>"] = ":cnext<CR>"
+lvim.keys.normal_mode["<C-k>"] = ":cprev<CR>"
 
 lvim.plugins = {
   {
